fdf57d2207bda49c4e52dd38f021673d8916155a
Navygeta <tinashenavonika@gmail.com>
2024-01-05 03:27:17 +0200

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,3 +19,31 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (balance_factor);
 }
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: 0 if tree is NULL, else return height.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0, max_height = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+			left_height = 1 + binary_tree_height(tree->left);
+
+		if (tree->right)
+			right_height = 1 + binary_tree_height(tree->right);
+
+		max_height = left_height;
+		if (right_height > left_height)
+			max_height = right_height;
+
+		return (max_height);
+	}
+	return (0);
+}
